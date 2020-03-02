1207ae3de00bdeb416918c8646add8903bf849bb
george kamau <flexkamau646@gmail.com>
2020-03-02 16:49:48 +0300

    added a quantity input in my html file

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ var number=document.getElementById("number").value
 alert(" hae " + name + " we've received your order it will be processed and deliverd to " +location)
 };
 
+function mySize(){
+    var size=document.getElementById('size');
+    return parseInt(size);
+}
+function myTopping(topping){
+    var topping=document.getElementById("topping");
+    return parseInt(topping);
+
+}
+function myCrust(crust){
+    var crust=document.getElementById("crust");
+    return parseInt(crust)
+}
