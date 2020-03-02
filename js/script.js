ea9e01b8d0e9d760aff2c76abfa655df0dde5497
george kamau <flexkamau646@gmail.com>
2020-03-02 14:41:35 +0300

    modified my styles file to add a paragraph describing each pizza

--- js/script.js
+++ js/script.js
@@ -9,6 +9,4 @@ var location=document.getElementById('location').value
 var number=document.getElementById("number").value
 alert(" hae " + name + " we've received your order it will be processed and deliverd to " +location)
 };
-function myOrder(size,type,topping,crust){
-    var size=[small,medium,large]
-}
+
