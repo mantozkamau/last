5fc1068a1ab0cc41e30b034a9e8baf0efcb5f8cb
george kamau <flexkamau646@gmail.com>
2020-03-02 13:38:22 +0300

    styled my script file to add an alert when the user places an order

--- js/script.js
+++ js/script.js
@@ -7,5 +7,5 @@ function myFunction(name,location,number){
 var name=document.getElementById("name").value
 var location=document.getElementById('location').value
 var number=document.getElementById("number").value
-alert('')
+alert(' hae ' + name +" we have received your order it will be processed within an hour")
 }
