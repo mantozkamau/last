da03ce2f2ef64f836173cb2b6f94c48f84a67984
george kamau <flexkamau646@gmail.com>
2020-03-02 10:52:31 +0300

    added a form in index.html to allow user enter their location and name

--- js/script.js
+++ js/script.js
@@ -2,7 +2,4 @@ $(document).ready(function () {
     $("#continue").click(function () {
         $("#close").slideToggle();
     });
-    $(".row").click(function(){
-        $(".row img").animate();
-    })
 });
