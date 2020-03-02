fb2b237e1604f1b19617b3461c94d7ddcefed92b
george kamau <flexkamau646@gmail.com>
2020-03-02 13:30:04 +0300

    added a background color to my form

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ $(document).ready(function () {
         $("#close").slideToggle();
     });
 });
+function myFunction(name,location,number){
+var name=document.getElementById("name").value
+var location=document.getElementById('location').value
+var number=document.getElementById("number").value
+alert('')
+}
