23c9d451b64ec4f1cfe6252b58e8856384dcc8f1
george kamau <flexkamau646@gmail.com>
2020-03-03 10:05:47 +0300

    added a form in my html file

--- js/script.js
+++ js/script.js
@@ -11,27 +11,3 @@ event.preventDefault();
 alert(" hae " + name + " we've received your order it will be processed and deliverd to " +location)
 };
 
-function mySize(){
-    var size = document.getElementById('size').value;
-    return parseInt(size);
-}
-function myTopping(topping){
-    var topping = document.getElementById("topping").value;
-    return parseInt(topping);
-
-}
-function myCrust(crust){
-    var crust = document.getElementById("crust").value;
-    return parseInt(crust)
-}
-function myNumber(){
-    var number = document.getElementById("quantity").value;
-    return parseInt(number);
-}
-function myFinal(){
-event.preventDefault();
-var total = 0;
-var total = (mySize()+ myTopping()+ myCrust()) * myNumber();
-console.log(total);
-alert("your order is " + total +"  your amount is " + total);
-};
