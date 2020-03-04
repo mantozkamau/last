9aa1fecd714c75d0faad90895a2f9c041ed13bcf
george kamau <flexkamau646@gmail.com>
2020-03-04 10:21:25 +0300

    deleted a commented code

--- js/script.js
+++ js/script.js
@@ -8,14 +8,20 @@ var name=document.getElementById("name").value
 var location=document.getElementById('location').value
 var number=document.getElementById("number").value
 event.preventDefault();
-alert(" hae " + name + " we've received your order it will be processed and deliverd to " +location)
+if(name===""||location===""||number===""){
+    alert("Fields can't be empty");
+}
+else{
+    alert(" hae " + name + " we've received your order it will be processed and deliverd to " +location)
+}
+
 };
 
 function myPrice(){
-    var crust=document.getElementById("crust").value
-    var topping=document.getElementById("topping").value
-    var size=document.getElementById("size").value
-    var quantity=document.getElementById("quantity").value
-    var final=(crust + topping +size ) *quantity;
-    alert("your order of " + crust + " , " + topping+ " , " + size + " Has been received and is being processed " + "  total price  is " + final );
- };
+   var crust=document.getElementById("crust").value
+   var topping=document.getElementById("topping").value
+   var size=document.getElementById("size").value
+   var quantity=document.getElementById("quantity").value
+   var final=(crust + topping +size ) *quantity;
+   alert("your order of " + crust + " , " + topping+ " , " + size + " Has been received and is being processed " + "  total price  is " + final );
+};
